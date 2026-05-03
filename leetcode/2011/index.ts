dc8a248a6526ecfdc0f1e206313b4ea31d6ce875
datdoan <datdoan.dev@gmail.com>
2026-05-04 02:19:12 +0700

    feat: implement finalValueAfterOperations using hash map reduction

--- leetcode/2011/index.ts
+++ leetcode/2011/index.ts
@@ -1,5 +1,15 @@
 /* https://leetcode.com/problems/final-value-of-variable-after-performing-operations/ */
 
-function finalValueAfterOperations(operations: string[]): number {}
-
-// console.log();
+function finalValueAfterOperations(operations: string[]): number {
+  const m = {
+    '--X': -1,
+    'X--': -1,
+    '++X': 1,
+    'X++': 1,
+  };
+  return operations.reduce((pre, cur) => {
+    return pre + m[cur];
+  }, 0);
+}
+console.log(finalValueAfterOperations(['--X', 'X++', 'X++']));
+console.log(finalValueAfterOperations(['X++', '++X']));
